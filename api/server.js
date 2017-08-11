a71e876da48b07205f108f63c9f83529f8725734
aliasmael <alilion22@ymail.com>
2017-08-11 19:45:39 +0200

    - [API] Body parsing

--- api/server.js
+++ api/server.js
@@ -1,10 +1,17 @@
 const express = require('express')  
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000;
 
 // use cors to allow host
 var cors = require('cors')
 app.use(cors())
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 // use category routes under /api/category
 app.use('/api/category', require('./routes/category').category);
