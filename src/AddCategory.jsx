0f733608da55115f12aaae8920ad8e055b4802fd
aliasmael <alilion22@ymail.com>
2017-08-11 19:47:20 +0200

    - [Category] Implement add new categor

--- src/AddCategory.jsx
+++ src/AddCategory.jsx
@@ -1,26 +1,72 @@
 import React from 'react';
 
 class AddCategory extends React.Component {
-   render() {
-      return (
-        <div className="ui segment">
-            <h4> Add Category </h4>
-            <form className="ui form" action="#">
-                <div className="eight fields">
-                    <div className="field">
-                        <input placeholder="English Name *" type="text" required="true" />
+
+    constructor(props) {
+        super(props);
+        this.state = { name: '', description: '' };
+        this.addNewCategory = this.addNewCategory.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+    }
+
+    vaildateForm () {
+        if ( this.state.name.trim() != '' )
+            return true;
+
+        return false;
+    }
+
+    addNewCategory (e) {
+        e.preventDefault();
+
+        if ( this.vaildateForm() ) {
+            var data = { categoryName: this.state.name, categoryDescription: this.state.description }
+            
+            $.ajax({
+                type: 'POST',
+                url: 'http://localhost:3000/api/category/add',
+                data: data
+            }).done(function(data) {
+                this.setState({ name: '', description: '' } );
+            })
+            .fail(function(jqXhr) {
+                console.log('failed to connect');
+            });
+
+        } else {
+            console.log("Error form ")
+        }
+    }
+
+    // handle user input changes
+    handleNameChange(e) {
+        this.setState({name: e.target.value});
+    }
+    handleDescriptionChange(e) {
+        this.setState({description: e.target.value});
+    }
+        
+    render() {
+        return (
+            <div className="ui segment">
+                <h4> Add Category </h4>
+                <form className="ui form" action="#">
+                    <div className="eight fields">
+                        <div className="field">
+                            <input placeholder="English Name *" type="text" name="category-name" value={this.state.name} onChange={this.handleNameChange} required />
+                        </div>
+                        <div className="field">
+                            <textarea placeholder="English Description" type="text" value={this.state.description} onChange={this.handleDescriptionChange} rows="1" />
+                        </div>
+                        <div className="field">
+                            <div className="ui positive submit button" type="submit" onClick={this.addNewCategory} >Create Category</div>
+                        </div>
                     </div>
-                    <div className="field">
-                        <textarea placeholder="English Description" type="text" rows="1" />
-                    </div>
-                    <div className="field">
-                        <div className="ui positive submit button" type="submit">Create Category</div>
-                    </div>
-                </div>
-            </form>
-        </div>
-      );
-   }
+                </form>
+            </div>
+        );
+    }
 }
 
 export default AddCategory;
